63fbf92670ec650fd6adb2de4e3e80b24e366274
Laurence Cortez <laurencecortez.com>
2023-05-21 10:29:43 +0800

    authenticate route, get user, and update user

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
