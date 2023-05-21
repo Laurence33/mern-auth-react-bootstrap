938c324b672d8268c27eca8a42ef4125ae9119ab
Laurence Cortez <laurencecortez.com>
2023-05-22 00:00:40 +0800

    react toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/header';
 import { Container } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <>
       <Header />
+      <ToastContainer />
       <Container>
         <Outlet />
       </Container>
